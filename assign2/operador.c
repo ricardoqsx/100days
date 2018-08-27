0d6d6793a36b697ca8e5bb80e5f65ad96229c8dc
ricardoqsx <ricardoqs@me.com>
2018-08-27 16:55:35 -0500

    program not finished

---
+++ assign2/operador.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define bv "\n\nBienvenido, este programa sumara o restara 2 numeros!\n\n"
+
+int main()
+
+{
+	char nm[10],ap[10];
+	float a,b,c,d,e;
+	printf(bv);
+
+	printf("Introduzca su nombre: ");
+	fgets(nombre,10,stdin);
+	printf("Ahora su apellido: ")
+	fgets(ap,10,stdin);
+
+	printf("Bien %s! Ahora prosigamos con el proceso:\n\n");
+
+	printf("Digite el primer numero: ");
+	scanf("%f",&a);
+	printf("Digite el segundo numero: ");
+	scanf("");
+
+
+return 0;
+}
