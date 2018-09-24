7bee0cd2a51c06e75a8ea0d13cda15ef45646483
Ricardo Quiroz S <Ricardoqsx@Lucy.local>
2018-09-24 00:00:56 -0500

    por fin funciona

--- assign3/auto.c
+++ assign3/auto.c
@@ -14,7 +14,7 @@ int main()
 	printf("Seleccione un tipo de auto: ");
 	printf("\n Opcion A: %s\n Opcion B: %s\n Opcion C: %s\n Opcion D: %s",a,b,c,d);
 	printf("\n\nSeleccione una opcion en minuscula: ");
-	scanf("%c",e);
+	scanf("%c",&e);
 //	setbuf(stdin,NULL);
 	fpurge(stdin);
 	printf("\nDiganos el precio del auto: ");
@@ -25,22 +25,22 @@ int main()
 	if(e=='a')
 	{
 		pcd=psd+(psd*0.10);
-		printf("\nComo nos comenta, el costo del vehiculo %s es de %.1f dolares, tendria que pagar %.1f",a,psd,pcd);
+		printf("\nComo nos comenta, el costo del vehiculo %s es de %.1f dolares,\ntendria que pagar %.1f\n\n",a,psd,pcd);
 	}
 	else if(e=='b')
 	{
 		pcd=psd+(psd*0.12);
-		printf("\nComo nos comenta, el costo del vehiculo %s es de %.1f dolares, tendria que pagar %.1f",b,psd,pcd);
+		printf("\nComo nos comenta, el costo del vehiculo %s es de %.1f dolares,\ntendria que pagar %.1f\n\n",b,psd,pcd);
 	}
 	else if(e=='c')
 	{
 		pcd=psd+(psd*0.05);
-		printf("\nComo nos comenta, el costo del vehiculo %s es de %.1f dolares, tendria que pagar %.1f",c,psd,pcd);
+		printf("\nComo nos comenta, el costo del vehiculo %s es de %.1f dolares,\ntendria que pagar %.1f\n\n",c,psd,pcd);
 	}
 	else if(e=='d')
 	{
 		pcd=psd+(psd*0.02);
-		printf("\nComo nos comenta, el costo del vehiculo %s es de %.1f dolares, tendria que pagar %.1f",d,psd,pcd);
+		printf("\nComo nos comenta, el costo del vehiculo %s es de %.1f dolares,\ntendria que pagar %.1f\n\n",d,psd,pcd);
 	}
 
 	return 0;
